5601314e44e9938505c0d74c1eadfa3cc814e01b
IBUNHABIBU <ibunhabib@gmail.com>
2024-12-29 16:59:05 +0300

    Add new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_29_133904) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_29_135637) do
   create_table "groups", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -31,6 +31,7 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_29_133904) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
   end
 
   create_table "users", force: :cascade do |t|
