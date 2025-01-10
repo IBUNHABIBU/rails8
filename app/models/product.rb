d5fab7408d6f175ed893d5d25d91c089fa46a8b7
IBUNHABIBU <ibunhabib@gmail.com>
2025-01-10 23:47:07 +0300

    Add subscription

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ApplicationRecord
+    has_many :subscribers, dependent: :destroy
     validates :name, presence: true
     has_rich_text :description
     has_one_attached :featured_image
+    validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
 end
