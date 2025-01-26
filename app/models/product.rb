4c434bb569883ec0709a132ba74936f2244144be
IBUNHABIBU <ibunhabib@gmail.com>
2025-01-26 20:47:44 +0300

    Add subscriber email

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,6 @@ class Product < ApplicationRecord
     has_rich_text :description
     has_one_attached :featured_image
     validates :inventory_count, numericality: { greater_than_or_equal_to: 0 }
+
+    
 end
