0663cab0c2595f0efbf10313119bc4110c19b5ef
IBUNHABIBU <ibunhabib@gmail.com>
2025-01-27 21:08:38 +0300

    Add notification concern

--- app/models/concerns/notification.rb
+++ app/models/concerns/notification.rb
@@ -2,6 +2,10 @@ module Product::Notifications
     extend ActiveSupport::Concern 
     include do
         has_many :subscribers, dependent: :destroy
-        after_update_commit :notify_ 
+        after_update_commit :notify_subscribers, if: :back_in_stock? 
+    end
+
+    def back_in_stock?
+        
     end
 end
