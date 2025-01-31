cdedc1ce79d5924f4332fd0cf6f7bdfec506d195
IBUNHABIBU <ibunhabib@gmail.com>
2025-01-31 22:21:41 +0300

    Add notification concern

--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -1,5 +1,6 @@
 class ProductMailer < ApplicationMailer
   # Subject can be set in your I18n file at config/locales/en.yml
+  
   # with the following lookup:
   #
   #   en.product_mailer.in_stock.subject
